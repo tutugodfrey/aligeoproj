a1cd10bbcc538fa84b5b8c5cf61d37f051924d37
tutu godfrey <godfrey_tutu@yahoo.com>
2017-07-31 21:43:12 +0100

    get two test passing

--- aligoejs/aligeofunc.js
+++ aligoejs/aligeofunc.js
@@ -1,6 +1,39 @@
 
 
-module.exports = function arigeo(ar) {
+module.exports = function arigeo(arrayvalue) {
+	if(arrayvalue.length === 0){
+		return 0;
+	}  else {
+		var arraycounter = 0;
+		let checker = arrayvalue[1] - arrayvalue[0];
+		var progressing = true;
+		var arithmetic = false;
+		var geometric = false;
+ 		var geometricratio;
 
+
+ 		for(let i = 0; i < arrayvalue.length; i++){
+ 			var secondcheck = arrayvalue[i + 1] -  arrayvalue[i];
+ 			if(secondcheck === checker){
+ 				arithmetic = true;
+ 			} else if (){}
+
+
+ 			
+
+
+ 		}//end for block
+
+
+
+	} //end else
+
+	if (arithmetic === true){
+		return "Arithmetic";
+	}
+	if (geometric === true ){
+		return "Geometric";
 
 	}
+
+}
--- testjs/aligeotest.js
+++ testjs/aligeotest.js
@@ -1,27 +1,34 @@
 const assert = require('chai').assert;
-const app= require('../aligoejs/aligeofunc.js');
+const arigeo = require('../aligoejs/aligeofunc.js');
 
 describe("aliGeo", function(){
 
-	describe("handle valid input", function(){
+
+	describe("handle invalid input", function(){
+
 		it ("should return 0 for []", function(){
-			assert.equal(app.arigeo([]), 0);
+			assert.equal(arigeo([]), 0)
 		});
-	});
-	describe("handle valid input", function(){
-		it ("should return 0 for [2, ]", function(){
-			assert.equal(app.arigeo([2, 4, 6, 8, 10]), 2);
+
+		it ("should return 0 for []", function(){
+			assert.equal(arigeo([2, 4, 5, 9, 10]), -1)
 		});
-	});
-	describe("handle valid input", function(){
-		it ("should return 0 for [2]", function(){
-			assert.equal(app.arigeo([2, 4, 8, 16, 32]), 2);
-		});
-	});
-	describe("handle valid input", function(){
-		it ("should return 0 for [2]", function(){
-			assert.equal(app.arigeo("term"), -1);
-		});
+
 	});
 
-})
+
+
+	describe("handle valid input", function(){
+
+		it ("should return 0 for []", function(){
+			assert.equal(arigeo([2, 4, 8, 16, 32]), "Geometric")
+		});
+
+		it ("should return 0 for []", function(){
+			assert.equal(arigeo([2, 4, 6, 8, 10]), "Arithmetic")
+		});
+
+	});
+
+		
+});
