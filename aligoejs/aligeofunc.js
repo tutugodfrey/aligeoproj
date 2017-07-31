0b0387f8d27157b711785c32354392235ce275b0
tutu godfrey <godfrey_tutu@yahoo.com>
2017-07-31 16:33:19 +0100

    test not passing

--- aligoejs/aligeofunc.js
+++ aligoejs/aligeofunc.js
@@ -1,9 +1,7 @@
 //let values = [2, 4, 6, 8]
 
 function arigeo(ar) {
-if (ar.length === 0 ){
-	return 0;
-}
+
 
 	
 
--- testjs/aligeotest.js
+++ testjs/aligeotest.js
@@ -4,12 +4,18 @@ const app= require('../aligoejs/aligeofunc.js');
 describe("aliGeo", function(){
 
 	describe("handle valid input", function(){
-		it ("should return 'Arithmetic' for []", function(){
+		it ("should return 0 for []", function(){
 			assert.equal(app.arigeo([]), 0);
 		});
 
+describe("handle valid input", function(){
+		it ("should return 0 for []", function(){
+			assert.equal(app.arigeo([]), 0);
+		});
 
-		
+	
+
+	
 
 	})
 
